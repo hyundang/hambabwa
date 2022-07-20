1e396f4a4f66c90a5c4e8a4144fe15644bbade12
hyunjin <hyunjin.l@21market.kr>
2022-07-20 18:37:20 +0900

    [chore] asset 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as checkIcon } from "./icn_check.svg";
+export { default as logoImg } from "./img_logo.svg";
+export { default as viewIcon } from "./icn_view.svg";
+export { default as hideIcon } from "./icn_hide.svg";
