2a109f71c9bb0556aba909baf635f744456fa156
hyunjin <hyunjin.l@21market.kr>
2022-07-27 14:30:48 +0900

    [feat] Like component

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -4,3 +4,4 @@ export { default as Input } from "./input";
 export { default as Category } from "./category";
 export { default as Star } from "./star";
 export { default as ProfileImg } from "./profileImg";
+export { default as Like } from "./like";
