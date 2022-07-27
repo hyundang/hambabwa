fa2ce4a211a88a847997b4694ab74d16939c13ea
hyunjin <hyunjin.l@21market.kr>
2022-07-27 14:30:34 +0900

    [feat] ProfileImg component

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -3,3 +3,4 @@ export { default as Checkbox } from "./checkbox";
 export { default as Input } from "./input";
 export { default as Category } from "./category";
 export { default as Star } from "./star";
+export { default as ProfileImg } from "./profileImg";
