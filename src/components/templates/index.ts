e587d0a48ecbc97b961555457d2a28b602a71e30
hyunjin <hyunjin.l@21market.kr>
2022-07-27 14:31:46 +0900

    [feat] MenuInfoList template

--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -1,3 +1,4 @@
 export { default as SignIn } from "./signin";
 export { default as SignUp } from "./signup";
 export { default as Profile } from "./profile";
+export { default as MenuInfoList } from "./menuInfoList";
