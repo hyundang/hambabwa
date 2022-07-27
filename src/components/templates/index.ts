9af017c5117fa3b5b7c8804c091be3f172d101a4
hyunjin <hyunjin.l@21market.kr>
2022-07-27 16:19:32 +0900

    [feat] RestaurantDetail template

--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -3,3 +3,4 @@ export { default as SignUp } from "./signup";
 export { default as Profile } from "./profile";
 export { default as MenuInfoList } from "./menuInfoList";
 export { default as CommentList } from "./commentList";
+export { default as RestaurantDetail } from "./restaurantDetail";
