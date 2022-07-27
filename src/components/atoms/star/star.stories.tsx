7ec67d225f2d711687ed2aec91af8b7d5e2fc4de
hyunjin <hyunjin.l@21market.kr>
2022-07-27 11:28:47 +0900

    [feat] Star compoent

---
+++ src/components/atoms/star/star.stories.tsx
@@ -0,0 +1,39 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Star from ".";
+
+export default {
+  title: "components/atoms/Star",
+  component: Star,
+  decorators: [
+    (story) => <div style={{ width: "295px", height: "196px" }}>{story()}</div>,
+  ],
+  parameters: {
+    actions: {
+      handles: ["click button"],
+    },
+  },
+} as ComponentMeta<typeof Star>;
+
+const Template: ComponentStory<typeof Star> = (args) => <Star {...args} />;
+
+export const DefaultStar = Template.bind({});
+DefaultStar.args = {
+  defaultValue: 3,
+};
+
+export const ReadOnlyStar = Template.bind({});
+ReadOnlyStar.args = {
+  defaultValue: 3,
+  readOnly: true,
+};
+
+export const EmptyStar = Template.bind({});
+EmptyStar.args = {
+  defaultValue: 0,
+};
+
+export const CustomSizeStar = Template.bind({});
+CustomSizeStar.args = {
+  defaultValue: 3,
+  size: 50
+};
