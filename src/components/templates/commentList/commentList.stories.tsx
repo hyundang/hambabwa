6557543652345b8022dc73c8b7023782dd53aa84
hyunjin <hyunjin.l@21market.kr>
2022-07-29 11:41:02 +0900

    [refactor] CommentList -> compound component 구조로 변경

--- src/components/templates/commentList/commentList.stories.tsx
+++ src/components/templates/commentList/commentList.stories.tsx
@@ -12,13 +12,36 @@ export default {
     },
   },
 } as ComponentMeta<typeof CommentList>;
- 
-const Template: ComponentStory<typeof CommentList> = (args) => (
-  <CommentList {...args} />
-);
 
-export const DefaultCommentList = Template.bind({});
+const DefaultTemplate: ComponentStory<typeof CommentList> = (args) => (
+  <CommentList {...args}>
+    <CommentList.TotalScore totalScore={4.5} />
+    <CommentList.DefaultComments nickname="" />
+  </CommentList>
+);
+export const DefaultCommentList = DefaultTemplate.bind({});
 DefaultCommentList.args = {
-  totalScore: 4.5,
+  comments: restaurantData.getComments,
+};
+
+const DetailTemplate: ComponentStory<typeof CommentList> = (args) => (
+  <CommentList {...args}>
+    <CommentList.TotalScore totalScore={4.5} displayTotalCount />
+    <CommentList.DefaultComments nickname="김지원" />
+  </CommentList>
+);
+export const CommentListWithTotalCount = DetailTemplate.bind({});
+CommentListWithTotalCount.args = {
+  comments: restaurantData.getComments,
+};
+
+const MypageTemplate: ComponentStory<typeof CommentList> = (args) => (
+  <CommentList {...args}>
+    <CommentList.TotalCount />
+    <CommentList.MyComments />
+  </CommentList>
+);
+export const MyCommentList = MypageTemplate.bind({});
+MyCommentList.args = {
   comments: restaurantData.getComments,
 };
--- src/components/templates/commentList/index.tsx
+++ src/components/templates/commentList/index.tsx
@@ -1,47 +1,165 @@
-import { HTMLAttributes, useEffect, useState } from "react";
-import { Star } from "src/components/atoms";
-import { Comment } from "src/components/organisms";
+import {
+  createContext,
+  HTMLAttributes,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
+import { Comment, ConfirmModal } from "src/components/organisms";
 import { restaurantTypes } from "src/types";
 import styled from "styled-components";
-import cookies from "react-cookies";
+import { Score } from "src/components/molecules";
+
+interface ContextProps extends HTMLAttributes<HTMLDivElement> {
+  comments?: restaurantTypes.menuCommentProps[];
+  handleClickDelete?: (cid: number) => () => void;
+}
+const CommentListContext = createContext<ContextProps>({});
+CommentListContext.displayName = "CommentListContext";
 
 interface CommentListProps extends HTMLAttributes<HTMLDivElement> {
-  totalScore: number;
   comments: restaurantTypes.menuCommentProps[];
+  onClickDelete: (cid: number) => void;
 }
 const CommentList = ({
-  id,
-  className,
-  totalScore,
+  children,
   comments,
+  onClickDelete,
 }: CommentListProps) => {
-  const [nickname, setNickname] = useState("");
-  useEffect(() => {
-    setNickname(cookies.load("nickname"));
-  }, []);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = (value: boolean) => () => setIsOpen(value);
+
+  const [commentId, setCommentId] = useState(-1);
+  const handleClickDelete = (cid: number) => () => {
+    setCommentId(cid);
+    setIsOpen(true);
+  };
+  const handleClickYes = () => {
+    onClickDelete(commentId);
+    setIsOpen(false);
+  };
+
+  const value = useMemo(
+    () => ({
+      comments,
+      handleClickDelete,
+    }),
+    [comments]
+  );
+
   return (
-    <CommentListWrap id={id} className={className}>
-      <TitleWrap>
-        <p>{totalScore}점</p>
-        <Star defaultValue={totalScore} readOnly size={22} />
-      </TitleWrap>
-      {comments.map((comment) => (
-        <Comment key={comment.id} comment={comment}>
-          {nickname === comment.writer.nickname ? (
-            <>
-              <Comment.MyProfileImg />
-              <Comment.Menu />
-            </>
-          ) : (
-            <Comment.DefaultProfileImg />
-          )}
-        </Comment>
-      ))}
-    </CommentListWrap>
+    <CommentListContext.Provider value={value}>
+      {children}
+      {isOpen && (
+        <ConfirmModal
+          text="후기를 삭제하시겠어요?"
+          setIsOpen={setIsOpen}
+          onClickNo={handleOpen(false)}
+          onClickYes={handleClickYes}
+        />
+      )}
+    </CommentListContext.Provider>
   );
 };
 
 export default CommentList;
+
+const useCommentList = () => {
+  const context = useContext(CommentListContext);
+  if (context === undefined)
+    throw new Error("useToggle must be used within a CommentList");
+  return context;
+};
+
+interface TotalScoreProps {
+  totalScore: number;
+  displayTotalCount?: boolean;
+}
+const TotalScore = ({ totalScore, displayTotalCount }: TotalScoreProps) => {
+  const { comments } = useCommentList();
+  return (
+    <Score
+      score={totalScore}
+      displayTotalCount={displayTotalCount}
+      totalCount={comments?.length}
+    />
+  );
+};
+
+const TotalCount = () => {
+  const { comments } = useCommentList();
+  return (
+    <CountWrap>
+      <p className="score_wrap">
+        전체 <b>{comments?.length}</b>
+      </p>
+    </CountWrap>
+  );
+};
+const CountWrap = styled.div`
+  width: 100%;
+  height: 34px;
+  padding: 0 25px;
+  border-bottom: 1px solid var(--gray_7);
+  line-height: 34px;
+  font-weight: 500;
+  font-size: 12px;
+  b {
+    font-weight: 600;
+    color: var(--red_1);
+  }
+`;
+
+const DefaultComments = ({ nickname }: { nickname: string }) => {
+  const { comments, handleClickDelete } = useCommentList();
+
+  return (
+    <CommentListWrap>
+      {comments?.length === 0 ? (
+        <p className="empty_text">작성된 후기가 없어요.</p>
+      ) : (
+        handleClickDelete &&
+        comments &&
+        comments.map((comment) => (
+          <Comment key={comment.id} comment={comment}>
+            {nickname === comment.writer.nickname ? (
+              <>
+                <Comment.MyProfileImg />
+                <Comment.Menu onClickDelete={handleClickDelete(comment.id)} />
+              </>
+            ) : (
+              <Comment.DefaultProfileImg />
+            )}
+          </Comment>
+        ))
+      )}
+    </CommentListWrap>
+  );
+};
+
+const MyComments = () => {
+  const { comments, handleClickDelete } = useCommentList();
+
+  return (
+    <CommentListWrap>
+      {comments?.length === 0 ? (
+        <p className="empty_text">작성된 후기가 없어요.</p>
+      ) : (
+        handleClickDelete &&
+        comments &&
+        comments.map((comment) => (
+          <Comment
+            key={comment.id}
+            comment={comment}
+            style={{ padding: "12px 25px 18px 18px" }}
+          >
+            <Comment.Menu onClickDelete={handleClickDelete(comment.id)} />
+          </Comment>
+        ))
+      )}
+    </CommentListWrap>
+  );
+};
 
 const CommentListWrap = styled.div`
   width: 100%;
@@ -49,18 +167,16 @@ const CommentListWrap = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-`;
 
-const TitleWrap = styled.div`
-  margin-bottom: 19px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  p {
-    height: 31px;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 35px;
-    margin-right: 10px;
+  .empty_text {
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+    color: var(--gray_2);
   }
 `;
+
+CommentList.TotalScore = TotalScore;
+CommentList.TotalCount = TotalCount;
+CommentList.DefaultComments = DefaultComments;
+CommentList.MyComments = MyComments;
