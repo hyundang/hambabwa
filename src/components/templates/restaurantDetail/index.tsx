f83626f62f11ce2f1629d19d485619309cef90b0
hyunjin <hyunjin.l@21market.kr>
2022-08-01 18:05:47 +0900

    [feat] 스크롤 감지 기능 추가

--- src/components/templates/restaurantDetail/index.tsx
+++ src/components/templates/restaurantDetail/index.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { emptyPinIcon, tagIcon } from "src/assets";
+import { Header } from "src/components/molecules";
 import { AskComment } from "src/components/organisms";
 import { restaurantTypes } from "src/types";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import CommentList from "../commentList";
 import MenuInfoList from "../menuInfoList";
 
@@ -31,8 +33,27 @@ const RestaurantDetail = ({
     router.push(`/comment/${router.query.id}`);
   };
 
+  const [isScrollActive, setIsScrollActive] = useState(false);
+  const handleScroll = () => {
+    if (window.scrollY > 180) setIsScrollActive(true);
+    else setIsScrollActive(false);
+  };
+  useEffect(() => {
+    const scrollListener = () =>
+      window.addEventListener("scroll", handleScroll);
+    scrollListener();
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <RestaurantDetailWrap>
+      <StyledHeader
+        isBackActive
+        title={isScrollActive ? restaurantInfo.name : ""}
+        isScrollActive={isScrollActive}
+      >
+        <Header.ProfileImg imageUrl={imageUrl} />
+      </StyledHeader>
       <ProfileWrap src={restaurantInfo.imageUrl}>
         <div className="profile">
           <p className="name">{restaurantInfo.name}</p>
@@ -113,6 +134,31 @@ const Text = styled.p`
 const RestaurantDetailWrap = styled.div`
   width: 100%;
   height: 100%;
+`;
+
+interface HeaderProps {
+  isScrollActive: boolean;
+}
+const StyledHeader = styled(Header)<HeaderProps>`
+  position: fixed;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  padding: 50px 23px 0 27px;
+  box-shadow: none;
+  ${({ isScrollActive }) =>
+    isScrollActive
+      ? css`
+          background-color: var(--white);
+        `
+      : css`
+          background-color: unset;
+          .back_icon {
+            path {
+              fill: var(--white);
+            }
+          }
+        `};
 `;
 
 interface ProfileWrapProps {
